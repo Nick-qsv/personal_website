d3866974b7c451a8038c5291a6b5d6b88ea2cc69
Nick Baez <w0m85k27@protonmail.com>
2022-08-26 20:58:36 -0400

    react 18

--- client/index.js
+++ client/index.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import {App} from './App'
-
-ReactDom.render(
-    <App />,
-    document.getElementById("app")
-)
+import React from "react";
+import { App } from "./App";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
